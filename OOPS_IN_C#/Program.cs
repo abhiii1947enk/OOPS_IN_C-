e9254c99fd69ac322d6f0c426b9d744eab50189d
abhiii1947enk <abhishek.n@enktechnologies.com>
2025-10-09 15:25:13 +0530

    Add project files.

---
+++ OOPS_IN_C#/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOPS_IN_C_
+{
+    internal class Program
+    {
+        public class Animal
+        {
+            public string skill;
+
+            public void Eat()
+            {
+                Console.WriteLine("Eat");
+            }
+        }
+
+        interface ILiving
+        {
+            void Live();
+        }
+
+        public abstract class Sound
+        {
+            public abstract void Play();
+            public void Play(string test)
+            {
+                Console.WriteLine($"{test}");
+            }
+        }
+
+        public class Cat : Sound
+        {
+            public override void Play()
+            {
+                throw new NotImplementedException();
+            }
+        }
+        
+
+        public class Dog : Animal, ILiving
+        {
+            public void Bark()
+            {
+                Console.WriteLine("Bark");
+            }
+
+            public void Live()
+            {
+                Console.WriteLine("Live");
+                Cat ct = new Cat();
+                ct.Play();
+                ct.Play("skill");
+            }
+           
+        }
+        static void Main(string[] args)
+        {
+            Dog objDog = new Dog();
+            objDog.Eat();
+            objDog.Bark();
+            objDog.Live();
+
+            Cat objCat = new Cat();
+            objCat.Play();
+        }
+    }
+}
